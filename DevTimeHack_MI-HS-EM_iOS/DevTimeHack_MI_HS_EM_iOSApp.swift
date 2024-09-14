777ec33cff6292630ee6137785c1db311024aa44
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-09-15 01:07:53 +0300

    Initial Commit

---
+++ DevTimeHack_MI-HS-EM_iOS/DevTimeHack_MI_HS_EM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevTimeHack_MI_HS_EM_iOSApp.swift
+//  DevTimeHack_MI-HS-EM_iOS
+//
+//  Created by Даниил Суханов on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevTimeHack_MI_HS_EM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
